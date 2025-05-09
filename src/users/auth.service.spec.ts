0f350f411a757b9bd166410b6b5ca5021d3005b7
sharathsDev <sharth.s@dignizant.com>
2025-05-09 18:17:56 +0530

    ADDED: unit testing for auth

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+
+    // // Mock the UsersService to avoid database calls
+    // const mockUsersService = {
+    //   find: () => Promise.resolve([]),
+    //   create: (name: string, email: string, password: string) => Promise.resolve({ id: '1', name, email, password }),
+    // };
+
+    // Mock the UsersService to avoid database calls
+    const mockUsersService = {
+      find: jest.fn(),
+      create: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, {
+        provide: UsersService,
+        useValue: mockUsersService,
+      }],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
